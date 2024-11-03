fa7c012bebd3d7923842d016175cb49a8a8c9153
Maggie Faust <mfaust@macalester.edu>
2024-11-03 14:55:42 -0600

    step 2

--- src/TimeTravel.elm
+++ src/TimeTravel.elm
@@ -4,6 +4,7 @@ import Playground exposing (..)
 import Set
 
 controlBarHeight = 64
+maxVisibleHistory = 2000
 
 addTimeTravel rawGame = 
     { initialState = initialStateWithTimeTravel rawGame
@@ -14,6 +15,7 @@ addTimeTravel rawGame =
 initialStateWithTimeTravel rawGame =
     { rawModel = rawGame.initialState
     , paused = False
+    , history = []
     }
 
 viewWithTimeTravel rawGame computer model =
@@ -23,9 +25,19 @@ viewWithTimeTravel rawGame computer model =
                 "press R to resume"
             else
                 "Press T to time travel"
+        historyBar color opacity index =
+            let
+                width = historyIndexToX computer index
+            in
+                rectangle color width controlBarHeight
+                    |> move (computer.screen.left + width / 2)
+                        (computer.screen.top - controlBarHeight / 2)
+                    |> fade opacity
     in
         (rawGame.view computer model.rawModel) ++
-            [ words white helpMessage
+            [ historyBar black 0.3 maxVisibleHistory
+            , historyBar (rgb 0 0 255) 0.6 (List.length model.history)
+            , words white helpMessage
                 |> move 0 (computer.screen.top - controlBarHeight / 2)    
             ]
 
@@ -39,7 +51,7 @@ updateWithTimeTravel rawGame computer model =
                 model.paused
     in
     if not model.paused then
-        {model | rawModel = rawGame.updateState computer model.rawModel, paused = newPaused}
+        {model | rawModel = rawGame.updateState computer model.rawModel, paused = newPaused, history = model.history ++ [computer]}
     else
         {model | paused = newPaused}
 
@@ -48,3 +60,6 @@ keyPressed keyName computer =
     , String.toUpper keyName
     ]
         |> List.any (\key -> Set.member key computer.keyboard.keys)
+
+historyIndexToX computer index =
+    (toFloat index) / maxVisibleHistory * computer.screen.width
